e19839ab3b635df63c66b25b2a342f83317cedc0
Christoph <mcteccy@gmail.com>
2019-12-23 23:15:32 +0100

    Added a little console based interface

--- PBDF-Extractor/Gui.Designer.cs
+++ PBDF-Extractor/Gui.Designer.cs
@@ -28,32 +28,43 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(13, 13);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// Gui
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(644, 450);
-			this.Controls.Add(this.button1);
-			this.Name = "Gui";
-			this.Text = "Gui";
-			this.ResumeLayout(false);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(539, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open Bv4";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Gui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Gui";
+            this.Text = "Gui";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
+        #endregion
 
-		private System.Windows.Forms.Button button1;
-	}
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
 }
--- PBDF-Extractor/Program.cs
+++ PBDF-Extractor/Program.cs
@@ -6,54 +6,202 @@ namespace PBDF_Extractor
 {
     class Program
     {
+        public static readonly string[] filetypes = { "Bv4", "Img" };
+
+        public static bool log = false;
+
         static void Main(string[] args)
         {
-            //LoadBl4();
-            LoadImg1();
-
-            Console.WriteLine("\n-----End-----");
-            while (true)
+            if(args.Length == 0)
             {
-                Thread.Sleep(20000);
+                StartWithoutParams();
+                return;
             }
+
+            // WIP
         }
 
-        static void LoadBv4()
+        static void StartWithoutParams()
         {
-            foreach (string file in Directory.EnumerateFiles(@"C:\Users\Christoph\Projekte\podFiles\POD GOLD\DATA\BINARY\VOITURES\"))
+            PrintSplash();
+            int filetype = ReadFileType();
+            if (filetype >= filetypes.Length)
             {
+                Console.WriteLine("Filetype not found");
+                return;
+            }
+
+            bool folder = ReadFolder();
+
+            string input = ReadInput(folder);
+            if (folder)
+                if (!Directory.Exists(input))
+                {
+                    Console.WriteLine("Input folder does not exist");
+                    return;
+                }
+                else
+                if (!File.Exists(input))
+                {
+                    Console.WriteLine("Input file does not exist");
+                    return;
+                }
+
+            string output = ReadOutput();
+            if (!Directory.Exists(output))
+            {
+                Console.WriteLine("Output folder does not exist");
+                return;
+            }
+
+            log = ReadLog();
+
+            if (folder)
+            {
+                if (filetype == 0)
+                {
+                    ConvertBv4Folder(input, output);
+                }
+                else if (filetype == 1)
+                {
+                    ConvertImgFolder(input, output);
+                }
+            }
+            else
+            {
+                if (filetype == 0)
+                {
+                    ConvertBv4File(input, output);
+                }
+                else if (filetype == 1)
+                {
+                    ConvertImgFile(input, output);
+                }
+            }
+
+            Console.WriteLine("\nPress any button to exit");
+            Console.ReadLine();
+        }
+
+        static void PrintSplash()
+        {
+            Console.WriteLine(Resources.Splash);
+            Console.WriteLine();
+            Console.WriteLine(Resources.SplashUnderline);
+            Console.WriteLine();
+            Console.WriteLine(Resources.Website);
+            Console.WriteLine();
+            Console.WriteLine(Resources.HelpHint);
+            Console.WriteLine();
+        }
+
+        static int ReadFileType()
+        {
+            Console.WriteLine(Resources.FiletypesHeadline);
+
+            for (int i = 0; i < filetypes.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + filetypes[i]);
+            }
+
+            return Convert.ToInt32(Console.ReadLine()) - 1;
+        }
+
+        static bool ReadFolder()
+        {
+            Console.WriteLine(Resources.Folder);
+            string s = Console.ReadLine();
+            if (s.Equals("y", StringComparison.OrdinalIgnoreCase))
+                return true;
+            return false;
+        }
+
+        static string ReadInput(bool folder)
+        {
+            Console.WriteLine(Resources.Input + (folder ? " folder: " : " file: "));
+            string s = Console.ReadLine();
+
+            if (folder)
+                if (!s.EndsWith("\\"))
+                    s += "\\";
+
+            return s;
+        }
+
+        static string ReadOutput()
+        {
+            Console.WriteLine(Resources.Output + " folder: ");
+            string s = Console.ReadLine();
+
+            if (!s.EndsWith("\\"))
+                s += "\\";
+
+            return s;
+        }
+
+        static bool ReadLog()
+        {
+            Console.WriteLine(Resources.Log);
+            string s = Console.ReadLine();
+            if (s.Equals("y", StringComparison.OrdinalIgnoreCase))
+                return true;
+            return false;
+        }
+
+        static void ConvertBv4Folder(string input, string output)
+        {
+            foreach (string file in Directory.EnumerateFiles(input))
+            {
+                string filename = file.Substring(file.LastIndexOf("\\") + 1);
+                Console.WriteLine("Converting " + filename);
+
                 Bv4 bv4 = new Bv4(file);
-                string s = bv4.name;
-                s = s.ToLower();
+                string s = bv4.name.ToLower();
                 s = s.Substring(0, 1).ToString().ToUpper() + s.Substring(1);
 
-                Console.WriteLine("fileNameToSave: " + @"C:\Users\Christoph\Projekte\podFiles\Cars\" + s);
-                bv4.SaveAllData(@"C:\Users\Christoph\Projekte\podFiles\Cars\" + s);
+                bv4.SaveAllData(output + s);
+
+                Console.WriteLine("done\r\n");
             }
         }
 
-        static void LoadBl4()
+        static void ConvertBv4File(string input, string output)
         {
-            Bl4 bl4 = new Bl4(@"C:\Users\Christoph\Projekte\podFiles\POD GOLD\DATA\BINARY\CIRCUITS\CITY.BL4");
+            string filename = input.Substring(input.LastIndexOf("\\") + 1);
+            Console.WriteLine("Converting " + filename);
+
+            Bv4 bv4 = new Bv4(input);
+            string s = bv4.name.ToLower();
+            s = s.Substring(0, 1).ToString().ToUpper() + s.Substring(1);
+
+            bv4.SaveAllData(output + s);
+
+            Console.WriteLine("done\r\n");
         }
 
-        static void LoadImg()
+        static void ConvertImgFolder(string input, string output)
         {
-            Img img = new Img(@"C:\Users\Christoph\Projekte\podFiles\POD GOLD\DATA\BINARY\MIMG\ALDERON.IMG");
-            img.SaveFiles(@"C:\Users\Christoph\Projekte\podFiles\Export\UI\ALDERON.IMG");
-        }
-
-        static void LoadImg1()
-        {
-            foreach (string file in Directory.EnumerateFiles(@"C:\Users\Christoph\Projekte\podFiles\POD GOLD\DATA\BINARY\MIMG\"))
+            foreach (string file in Directory.EnumerateFiles(input))
             {
+                string filename = file.Substring(file.LastIndexOf("\\") + 1);
+                Console.WriteLine("Converting " + filename);
 
-                Console.WriteLine(file);
                 Img img = new Img(file);
-                string s = file.Substring(file.LastIndexOf("\\") + 1);
-                //img.SaveFiles(@"C:\Users\Christoph\Projekte\podFiles\Export\UI\" + s);
-                Console.WriteLine("\tdone");
+                img.SaveFiles(output + filename);
+
+                Console.WriteLine("done\r\n");
             }
+        }
+
+        static void ConvertImgFile(string input, string output)
+        {
+            string filename = input.Substring(input.LastIndexOf("\\") + 1);
+            Console.WriteLine("Converting " + filename);
+
+            Img img = new Img(input);
+            img.SaveFiles(output + filename);
+
+            Console.WriteLine("done\r\n");
         }
     }
 }
